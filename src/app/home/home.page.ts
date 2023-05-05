5b428cf0cc964f66d31f759ebf793b4d1394d848
guiplacie <125033612+guiplacie@users.noreply.github.com>
2023-05-05 15:57:05 +0000

    pagina de produtos atualizada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,18 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule , CommonModule],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage{
+  produtos = [
+    {id:0, nome: 'PS4', preco: 'R$ 2000,00',  marca: 'Sony', image: 'https://images.pexels.com/photos/1082810/pexels-photo-1082810.jpeg?auto=compress&cs=tinysrgb&w=1600' },
+    {id:1, nome: 'PS5', preco: 'R$ 5000,00', marca: 'Sony', image:'https://images.pexels.com/photos/14824332/pexels-photo-14824332.jpeg?auto=compress&cs=tinysrgb&w=1600'},
+    {id:2, nome: 'Polisatation', preco: 'R$ 200,00', marca: 'Polishop', image:'https://images.pexels.com/photos/2728255/pexels-photo-2728255.jpeg?auto=compress&cs=tinysrgb&w=1600'},
+    {id:3, nome: 'Super Nitendo', preco: 'R$ 900,00', marca: 'Nitendo', image:'https://images.pexels.com/photos/4842495/pexels-photo-4842495.jpeg?auto=compress&cs=tinysrgb&w=1600'},
+    {id:4, nome: 'Super Nes', preco: 'R$ 250,00', marca: 'Super Tech', image:'https://images.pexels.com/photos/1367036/pexels-photo-1367036.jpeg?auto=compress&cs=tinysrgb&w=1600'},
+    {id:5, nome: 'MegaDrive', preco: 'R$ 800,00', marca: 'Sega', image:'https://images.pexels.com/photos/163036/mario-luigi-yoschi-figures-163036.jpeg?auto=compress&cs=tinysrgb&w=1600'},
+  ];
+  
+
+  alertButtons = ['OK'];
 }
